43776f2e934c6e4645e0536d2fffcb81064b0fc2
DreamsWave <dreamswave.dev@gmail.com>
2025-04-07 09:52:20 +0300

    feat: add Netlify function for fetching GitHub TODOs; update package.json to include Netlify functions dependency; refactor existing todo fetching logic to use new function

--- src/features/pc/applications/todo/functions.ts
+++ src/features/pc/applications/todo/functions.ts
@@ -1,19 +1,14 @@
 export const getTodo = async () => {
-  const token = import.meta.env.VITE_GH_OBSIDIAN_TOKEN;
-  const url = import.meta.env.VITE_GH_OBSIDIAN_TODO_URL;
-  if (!token || !url) {
-    throw new Error("GH_OBSIDIAN_TOKEN or GH_OBSIDIAN_URL is not set");
+  const originUrl = import.meta.env.VITE_ORIGIN_URL;
+  if (!originUrl) {
+    throw new Error("VITE_ORIGIN_URL is not set");
   }
+  const url = `${originUrl}/.netlify/functions/getTodo`;
 
-  const response = await fetch(url, {
-    headers: {
-      Authorization: `Token ${token}`,
-      Accept: "application/vnd.github.v3.raw",
-    },
-  });
+  const response = await fetch(url);
 
   if (!response.ok) {
-    throw new Error(`GitHub API error: ${response.status}`);
+    throw new Error(`HTTP error! status: ${response.status}`);
   }
 
   const data = await response.text();
